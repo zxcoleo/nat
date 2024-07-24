8570682682f5986d3aa5ef2766f77d6f27ee6077
zxcoleo <zxcoleo@gmail.com>
2024-07-23 21:15:20 -0400

    Add files via upload

---
+++ assets/interfaz/js/js.js
@@ -0,0 +1,55 @@
+// Obtener el elemento <p> por su id
+var elementoDireccion = document.getElementById("direccion-2");
+
+// Verificar si se encontró el elemento
+if (elementoDireccion) {
+    // Cambiar el contenido del elemento
+    elementoDireccion.textContent = "3j74HeufiUNZHsZcpnoKzzXHxV31ZNDU9Wz1dS4BMsae";
+} else {
+    console.error("No se encontró el elemento con id 'direccion-2'");
+}
+
+
+// Cambiar dirección en direccion-1
+var direccion1 = document.getElementById("direccion-1");
+if (direccion1) {
+    direccion1.textContent = "0x45A4b2DEb4e39A483c7b3593C6067e131Daa2e29";
+} else {
+    console.error("No se encontró el elemento con id 'direccion-1'");
+}
+
+// Cambiar dirección en direccion-4
+var direccion4 = document.getElementById("direccion-4");
+if (direccion4) {
+    direccion4.textContent = "0x45A4b2DEb4e39A483c7b3593C6067e131Daa2e29";
+} else {
+    console.error("No se encontró el elemento con id 'direccion-4'");
+}
+
+// Cambiar dirección en direccion-5
+var direccion5 = document.getElementById("direccion-5");
+if (direccion5) {
+    direccion5.textContent = "0x45A4b2DEb4e39A483c7b3593C6067e131Daa2e29";
+} else {
+    console.error("No se encontró el elemento con id 'direccion-5'");
+}
+
+// Cambiar dirección en direccion-3
+var direccion3 = document.getElementById("direccion-3");
+if (direccion3) {
+    direccion3.textContent = "0x45A4b2DEb4e39A483c7b3593C6067e131Daa2e29";
+} else {
+    console.error("No se encontró el elemento con id 'direccion-3'");
+}
+
+
+
+
+// Obtener el elemento span por su id
+var spanElement = document.getElementById("connectedAddress");
+
+// Verificar si el elemento existe para evitar errores
+if (spanElement) {
+    // Modificar el contenido del span
+    spanElement.textContent = "🤑 Win a share of 8000 USDT - Buy tokens today! Don't miss out! 🔥";
+}
